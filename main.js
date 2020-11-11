e71ae91cb2e9112f35162f7b131d2d4348820954
Francesco <fraancesco.bello@gmail.com>
2020-11-11 16:42:59 +0100

    end

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ var app = new Vue(
     el : "#root",
     data : {
       items : ["fare la spesa",  "uscire il cane", "lavare la macchina", "comprare il pane"],
-      new :[]
+      newTodo : ""
     },
     methods : {
       addItem () {
-        this.items.push(this.new);
-        this.new = "";
+        this.items.push(this.newTodo);
+        this.newTodo = "";
+      },
+      removeItem: function (index) {
+        this.items.splice(index, 1);
+
       }
     },
 
