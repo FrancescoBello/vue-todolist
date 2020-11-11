41026ee57a858e054e89c9357af8b06fc203984b
Francesco <fraancesco.bello@gmail.com>
2020-11-11 12:48:39 +0100

    aggiunto input per altri todo list

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ var app = new Vue(
     el : "#root",
     data : {
       items : ["fare la spesa", "uscire il cane", "lavare la macchina", "comprare il pane"]
-    }
+    },
+    methods : {
+      addItem () {
+        this.items.push(this.new);
+        this.new = "";
+      }
+    },
+
   }
 )
